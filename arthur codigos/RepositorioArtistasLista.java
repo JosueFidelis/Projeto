17feaa6998897b79ff8a2b0f7f631ac4793e58e8
acrc2 <32888196+acrc2@users.noreply.github.com>
2017-11-20 16:34:06 -0300

    Add files via upload

--- arthur codigos/RepositorioArtistasLista.java
+++ arthur codigos/RepositorioArtistasLista.java
@@ -1,4 +1,69 @@
 
-public class RepositorioArtistasLista {
+
+public class RepositorioArtistasLista implements RepositorioArtistas {
+	private Artista artista;
+	private RepositorioArtistasLista proximo;
+
+	public String inserirArtista(Artista artista)throws ANEException {
+		boolean j=false;
+		if (this.artista!=null) {
+			if (this.artista==artista) {
+				j=true;
+			}else {
+			
+			}
+		}
+		
+		if (this.artista == null) {
+			this.artista = artista;
+			this.proximo = new RepositorioArtistasLista();
+			return "Artista adicionado!!";
+		} else {
+			this.proximo.inserirArtista(artista);
+		}
+	}
+
+	public String removerArtista(Artista artista) throws ANEException{
+		if (this.artista != null) {
+			if (this.artista.equals(artista)) {
+				this.artista = this.proximo.artista;
+				this.proximo = this.proximo.proximo;
+				return "artista removido com sucesso";
+			} else {
+				return this.proximo.removerArtista(artista);
+			}
+		}else {
+			ANEException e;
+			e=new ANEException(artista.getNome());
+			return e.getMessage();
+		}
+	}
+
+	public String buscarArtista(Artista artista) throws ANEException{
+		if (this.artista != null) {
+			if(this.artista==artista) {
+				
+				return "Artista encontrado";
+			}else {
+				return this.proximo.buscarArtista(artista);
+			}
+
+		}else {
+			ANEException e;
+			e = new ANEException(artista.getNome());
+			return e.getMessage();
+		}
+
+	}
 
 }
+
+// meu amor te amo
+// •cj cantor☻
+// System.out.print ► 0:18______________2:35
+
+// PARA SA PORRA
+
+// meu amor te amo
+// •cj cantor☻
+// System.out.print ■ 0:18______________2:35
