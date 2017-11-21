310fea2496f7024ee00a4ca52a224cb70f4b7f03
AdrianoCabral <32920783+AdrianoCabral@users.noreply.github.com>
2017-11-21 11:00:56 -0300

    Update RepositorioStringLista.java

--- hm sua irmã/repositorio/RepositorioStringLista.java
+++ hm sua irmã/repositorio/RepositorioStringLista.java
@@ -1,11 +1,37 @@
 package repositorio;
 
+import interfaces.RepositorioString;
+
 public class RepositorioStringLista implements RepositorioString{
 	private String nome;
 	private RepositorioStringLista proximo;
 	
+	public RepositorioStringLista() {
+		this.nome = null;
+		this.proximo = null;
+	}
 	public String imprimir() {
-		return "hm sua irm„";
+		return "hm sua irm√£";
+	}
+
+	public void inserir(String x) {
+		if(this.nome == null) {
+			this.nome = x;
+			this.proximo = new RepositorioStringLista();
+		}else {
+			this.proximo.inserir(x);
+		}
+	}
+
+	public void remover(String x) {
+		if(this.nome == null) {
+			return;
+		}else if(this.nome.equals(x)){
+			this.nome = this.proximo.nome;
+			this.proximo = this.proximo.proximo;
+		}else {
+			this.proximo.remover(x);
+		}
 	}
 	
 }
