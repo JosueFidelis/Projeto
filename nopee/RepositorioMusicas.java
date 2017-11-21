6bb17a3814df203d5bf4bd2b4a0e27363a0cb30b
gkqsj <32937732+gkqsj@users.noreply.github.com>
2017-11-21 16:58:24 -0300

    Add files via upload

---
+++ nopee/RepositorioMusicas.java
@@ -0,0 +1,11 @@
+package nopee;
+
+public interface RepositorioMusicas {
+	public void inserir(Musica musica);
+
+	public void remover(Musica musica);
+
+	public void tocar(Musica musica);
+
+	public void buscar(Musica musica);
+}
