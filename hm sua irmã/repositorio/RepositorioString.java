2ded96baf7663dd077ab6fd6dcab0520d91a3f8c
AdrianoCabral <32920783+AdrianoCabral@users.noreply.github.com>
2017-11-21 23:43:50 -0300

    Update RepositorioString.java

--- hm sua irmã/repositorio/RepositorioString.java
+++ hm sua irmã/repositorio/RepositorioString.java
@@ -1,8 +1,10 @@
 package repositorio;
 
+import excecoes.Erou;
+
 public interface RepositorioString {
-	String imprimir();
-	void inserir(String x);
-	void remover(String x);
+	String imprimir(int i);
+	void inserir(String x, int i);
+	String remover(String x, int i) throws Erou;
 
 }
