6046f3abab83bbe76523296da9d47bfcda3c288c
aldibergjr <aldibergjr@gmail.com>
2017-11-23 15:40:37 -0300

    Add files via upload

--- Codes Berg/Negocio/NegocioPlaylist.java
+++ Codes Berg/Negocio/NegocioPlaylist.java
@@ -1,5 +1,29 @@
 package Negocio;
-
+import Exceptions.PlaylistNaoEncontradaException;
+import Playlists.Playlist;
+import Playlists.RepositorioPlaylists;
+import Playlists.RepositorioPlaylistsArray;
+import Playlists.RepositorioPlaylistsLista;
 public class NegocioPlaylist {
-
+	private RepositorioPlaylists list;
+	
+	public NegocioPlaylist(boolean islist)
+	{
+		if(islist = true)
+			list = new RepositorioPlaylistsLista();
+		else
+			list =new RepositorioPlaylistsArray();
+	}
+	
+	public Playlist buscarPlaylist(String nome) throws PlaylistNaoEncontradaException{
+		return list.buscar(nome);
+	}
+	public void inserirPlaylist(String nome,boolean ispremium)
+	{
+		list.inserir(nome, ispremium);
+	}
+	public void removerPlaylist(String nome) throws PlaylistNaoEncontradaException{
+		list.remover(nome);
+	}
+	
 }
