9ea23c24958e77cb11f994f4b60d0e9983b80a8d
AdrianoCabral <32920783+AdrianoCabral@users.noreply.github.com>
2017-11-23 15:45:56 -0300

    Add files via upload

--- hm sua irmã/repositorio/RepositorioContas.java
+++ hm sua irmã/repositorio/RepositorioContas.java
@@ -1,11 +1,14 @@
-package repositorio;
+package interfaces;
 
-import excecoes.Erou;
+import excecoes.ErouException;
 import user.ClasseAbstrataUser;
+import excecoes.ComandoInvalidoException;
 
 public interface RepositorioContas {
 	String inserir(ClasseAbstrataUser conta, int i);
-	String remover(ClasseAbstrataUser conta, int i)throws Erou;
-	boolean procurar(ClasseAbstrataUser conta, int i);
-	
+
+	String remover(String x, int i) throws ErouException;
+
+	ClasseAbstrataUser procurar(String x, int i) throws ErouException;
+
 }
--- hm sua irmã/repositorio/RepositorioString.java
+++ hm sua irmã/repositorio/RepositorioString.java
@@ -1,10 +1,12 @@
-package repositorio;
+package interfaces;
 
-import excecoes.Erou;
+import excecoes.ErouException;
 
 public interface RepositorioString {
 	String imprimir(int i);
+
 	void inserir(String x, int i);
-	String remover(String x, int i) throws Erou;
+
+	String remover(String x, int i) throws ErouException;
 
 }
