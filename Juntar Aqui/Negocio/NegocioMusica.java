ed753d09cd368812dc5cb5611b21d2a3211c4802
gkqsj <32937732+gkqsj@users.noreply.github.com>
2017-11-27 17:26:51 -0200

    Add files via upload

---
+++ Juntar Aqui/Negocio/NegocioMusica.java
@@ -0,0 +1,32 @@
+package Negócios;
+
+import Exceptions.NaoEncontradaException;
+import nopee.*;
+
+public class NegocioMusica {
+	private RepositorioMusicas lista;
+
+	public NegocioMusica(boolean a) {
+		if (a) {
+			this.lista = new RepositorioMusicaLista();
+		} else {
+			this.lista = new RepositorioMusicaArray();
+		}
+	}
+
+	public String inserir(Musica musica) {
+		return lista.inserir(musica);
+	}
+
+	public String remover(Musica musica) throws NaoEncontradaException {
+		return lista.remover(musica);
+	}
+
+	public boolean buscar(Musica musica) throws NaoEncontradaException {
+		return lista.buscar(musica);
+	}
+
+	public String tocar(Musica musica) throws NaoEncontradaException {
+		return lista.tocar(musica);
+	}
+}
